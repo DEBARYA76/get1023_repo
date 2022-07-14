666801bfab2f9091625c48c7761436bf0f91c221
Debarya Pal <75879603+DEBARYA76@users.noreply.github.com>
2022-07-14 17:16:42 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,7 +2,19 @@ class sample
 {
 public static void main(String[] args)
 {
-System.out.println("Hello worlds");
+System.out.println("Hello world");
+  System.out.println("Hello world");
+  System.out.println("Hello world");
+  
+  int i,j;
+  for(i=0;i<5;i++)
+  {
+    for(j=0;j<5;j++)
+      System.out.println("&&&&    ");
+    
+    System.out.println("@@@@@    ");
+  }
+  
 
 
 
